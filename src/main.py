4087bc66f74cf328216430591f948347015155cb
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2019-04-12 18:47:09 +0100

    Initial REST API commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import ina219
 import usocket as socket
+import ujson as json
 import utime as time
 
 start = time.ticks_ms()
@@ -8,17 +9,13 @@ energy = 0
 
 
 def res():
-    html = """<!DOCTYPE html>
-        <html>
-            <head><title>READINGS</title></head>
-            <body><table border="1">
-                <tr><td>CURRENT (A)</td><td>""" + str(current) + """</td></tr>
-                <tr><td>VOLTAGE (V)</td><td>""" + str(voltage) + """</td></tr>
-                <tr><td>POWER (W)</td><td>""" + str(power) + """</td></tr>
-                <tr><td>TOTAL ENERGY (J)</td><td>""" + str(energy) + """</td></tr>
-            </table></body>
-        </html>"""
-    return html
+    data = {
+        "currentInA": current,
+        "voltageInV": voltage,
+        "powerInW": power,
+        "energyTotalInJ": energy,
+    }
+    return json.dumps(data)
 
 
 addr = socket.getaddrinfo('192.168.4.1', 80)[0][-1]
